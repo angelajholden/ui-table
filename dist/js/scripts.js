a91dadaf5b3e8fa1d770feb7107b3fa022e9dca9
Angela J. Holden <hold4477@stthomas.edu>
2021-01-20 16:12:36 -0600

    adding some js to make it dynamic

--- dist/js/scripts.js
+++ dist/js/scripts.js
@@ -1,9 +1,87 @@
+function tableData() {
+	let request = new XMLHttpRequest();
+	request.open("GET", "https://jsonplaceholder.typicode.com/users");
+	request.onload = () => {
+		let response = request.response;
+		let parseData = JSON.parse(response);
+
+		let table = document.querySelector(".table");
+
+		let keys = Object.keys(parseData[0]);
+		// console.log(keys.join(", "));
+
+		let headName = keys[1];
+		let headUser = keys[2];
+		let headEmail = keys[3];
+		let headAddress = keys[4];
+		let headPhone = keys[5];
+
+		let tableHead = `
+		    <th scope="col">${headName}</th>
+		    <th scope="col">${headUser}</th>
+		    <th scope="col">${headEmail}</th>
+		    <th scope="col">${headAddress}</th>
+		    <th scope="col">${headPhone}</th>
+		`;
+
+		let tr = document.getElementById("tr");
+		if (table) {
+			tr.innerHTML = tableHead;
+		}
+
+		let tableBody = "";
+		for (let i = 0; i < parseData.length; i++) {
+			let name = parseData[i].name;
+			let username = parseData[i].username;
+			let email = parseData[i].email;
+			let phone = parseData[i].phone;
+
+			let street = parseData[i].address.street;
+			let suite = parseData[i].address.suite;
+			let city = parseData[i].address.city;
+			let zip = parseData[i].address.zipcode;
+
+			let address = `
+                <div>${street}</div>
+                <div>${suite}</div>
+                <div>${city}, ${zip}</div>
+            `;
+
+			let data = Object.keys(parseData[i]);
+			// console.log(data.join(", "));
+
+			let dataName = data[1];
+			let dataUser = data[2];
+			let dataEmail = data[3];
+			let dataAddress = data[4];
+			let dataPhone = data[5];
+
+			tableBody += `
+            <tr>
+                <th data-label="${dataName}" scope="row">${name}</th>
+                <td data-label="${dataUser}">${username}</td>
+                <td data-label="${dataEmail}">${email}</td>
+                <td data-label="${dataAddress}">${address}</td>
+                <td data-label="${dataPhone}">${phone}</td>
+            </tr>
+            `;
+		}
+
+		let tbody = document.getElementById("tbody");
+		if (table) {
+			tbody.innerHTML = tableBody;
+		}
+	};
+	request.send();
+}
+
 function copyrightYear() {
-    let c = document.getElementById("date");
-    let d = new Date();
-    c.innerHTML = d.getFullYear();
+	let c = document.getElementById("date");
+	let d = new Date();
+	c.innerHTML = d.getFullYear();
 }
 
 window.onload = () => {
-    copyrightYear();
+	tableData();
+	copyrightYear();
 };
