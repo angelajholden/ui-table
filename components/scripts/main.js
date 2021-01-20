722dad3d2240e96a720b4bd0dea4001472e68a1d
Angela J. Holden <hold4477@stthomas.edu>
2021-01-20 17:26:34 -0600

    convert to tabs

--- components/scripts/main.js
+++ components/scripts/main.js
@@ -8,7 +8,6 @@ function tableData() {
 		const table = document.querySelector(".table");
 
 		const keys = Object.keys(parseData[0]);
-		// console.log(keys.splice(1, 5));
 
 		const headName = keys[1];
 		const headUser = keys[2];
@@ -17,11 +16,11 @@ function tableData() {
 		const headPhone = keys[5];
 
 		const tableHead = `
-		    <th scope="col">${headName}</th>
-		    <th scope="col">${headUser}</th>
-		    <th scope="col">${headEmail}</th>
-		    <th scope="col">${headAddress}</th>
-		    <th scope="col">${headPhone}</th>
+			<th scope="col">${headName}</th>
+			<th scope="col">${headUser}</th>
+			<th scope="col">${headEmail}</th>
+			<th scope="col">${headAddress}</th>
+			<th scope="col">${headPhone}</th>
 		`;
 
 		const tr = document.getElementById("tr");
@@ -42,20 +41,20 @@ function tableData() {
 			const zip = parseData[i].address.zipcode;
 
 			const address = `
-                <div>${street}</div>
-                <div>${suite}</div>
-                <div>${city}, ${zip}</div>
-            `;
+				<div>${street}</div>
+				<div>${suite}</div>
+				<div>${city}, ${zip}</div>
+			`;
 
 			tableBody += `
-                <tr>
-                    <th data-label="${headName}" scope="row">${name}</th>
-                    <td data-label="${headUser}">${username}</td>
-                    <td data-label="${headEmail}">${email}</td>
-                    <td data-label="${headAddress}">${address}</td>
-                    <td data-label="${headPhone}">${phone}</td>
-                </tr>
-            `;
+				<tr>
+					<th data-label="${headName}" scope="row">${name}</th>
+					<td data-label="${headUser}">${username}</td>
+					<td data-label="${headEmail}">${email}</td>
+					<td data-label="${headAddress}">${address}</td>
+					<td data-label="${headPhone}">${phone}</td>
+				</tr>
+			`;
 		}
 
 		const tbody = document.getElementById("tbody");
