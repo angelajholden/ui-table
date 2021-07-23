55af24716cd87a4559d2e60454af12a6b2cea960
Angela J. Holden <hold4477@stthomas.edu>
2021-07-23 09:00:19 -0500

    updating repo

--- components/scripts/main.js
+++ components/scripts/main.js
@@ -8,6 +8,7 @@ function tableData() {
 		const table = document.querySelector(".table");
 
 		const keys = Object.keys(parseData[0]);
+		// console.log(keys.splice(1, 5));
 
 		const headName = keys[1];
 		const headUser = keys[2];
@@ -15,17 +16,17 @@ function tableData() {
 		const headAddress = keys[4];
 		const headPhone = keys[5];
 
-		const tableHead = `
+		const tabelHead = `
 			<th scope="col">${headName}</th>
-			<th scope="col">${headUser}</th>
-			<th scope="col">${headEmail}</th>
-			<th scope="col">${headAddress}</th>
-			<th scope="col">${headPhone}</th>
+            <th scope="col">${headUser}</th>
+            <th scope="col">${headEmail}</th>
+            <th scope="col">${headAddress}</th>
+            <th scope="col">${headPhone}</th>
 		`;
 
 		const tr = document.getElementById("tr");
 		if (table) {
-			tr.innerHTML = tableHead;
+			tr.innerHTML = tabelHead;
 		}
 
 		let tableBody = "";
@@ -42,18 +43,18 @@ function tableData() {
 
 			const address = `
 				<div>${street}</div>
-				<div>${suite}</div>
-				<div>${city}, ${zip}</div>
+                <div>${suite}</div>
+                <div>${city}, ${zip}</div>
 			`;
 
 			tableBody += `
 				<tr>
-					<th data-label="${headName}" scope="row">${name}</th>
-					<td data-label="${headUser}">${username}</td>
-					<td data-label="${headEmail}">${email}</td>
-					<td data-label="${headAddress}">${address}</td>
-					<td data-label="${headPhone}">${phone}</td>
-				</tr>
+                    <th data-label="${headName}" scope="row">${name}</th>
+                    <td data-label="${headUser}">${username}</td>
+                    <td data-label="${headEmail}">${email}</td>
+                    <td data-label="${headAddress}">${address}</td>
+                    <td data-label="${headPhone}">${phone}</td>
+                </tr>
 			`;
 		}
 
