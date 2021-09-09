c64af3628866ac38454664b75adce90a13ec6a68
Angela J. Holden <angelajholden@gmail.com>
2021-09-08 21:07:34 -0500

    updating to es6

--- components/scripts/main.js
+++ components/scripts/main.js
@@ -6,60 +6,36 @@ function tableData() {
 		const parseData = JSON.parse(response);
 
 		const table = document.querySelector(".table");
+		const keys = Object.keys(parseData[0]).splice(1, 5);
+		// console.log(keys);
 
-		const keys = Object.keys(parseData[0]);
+		let headItems = [];
+		keys.forEach((key) => {
+			headItems.push(`<th scope=\"col\">${key}</th>`);
+		});
+		// console.log(headItems);
 
-		const headName = keys[1];
-		const headUser = keys[2];
-		const headEmail = keys[3];
-		const headAddress = keys[4];
-		const headPhone = keys[5];
-
-		const tableHead = `
-			<th scope="col">${headName}</th>
-			<th scope="col">${headUser}</th>
-			<th scope="col">${headEmail}</th>
-			<th scope="col">${headAddress}</th>
-			<th scope="col">${headPhone}</th>
-		`;
+		let bodyItems = [];
+		parseData.forEach((data) => {
+			bodyItems.push(`<tr>
+				<th data-label=\"${keys[0]}\" scope=\"row\">${data.name}</th>
+				<td data-label=\"${keys[1]}\">${data.username}</td>
+				<td data-label=\"${keys[2]}\">${data.email}</td>
+				<td data-label=\"${keys[3]}\">
+					${data.address.street}<br/>
+					${data.address.suite}<br/>
+					${data.address.city}, ${data.address.zipcode}
+				</td>
+				<td data-label=\"${keys[4]}\">${data.phone}</td>
+			</tr>`);
+		});
+		// console.log(bodyItems);
 
 		const tr = document.getElementById("tr");
-		if (table) {
-			tr.innerHTML = tableHead;
-		}
-
-		let tableBody = "";
-		for (let i = 0; i < parseData.length; i++) {
-			const name = parseData[i].name;
-			const username = parseData[i].username;
-			const email = parseData[i].email;
-			const phone = parseData[i].phone;
-
-			const street = parseData[i].address.street;
-			const suite = parseData[i].address.suite;
-			const city = parseData[i].address.city;
-			const zip = parseData[i].address.zipcode;
-
-			const address = `
-				<div>${street}</div>
-				<div>${suite}</div>
-				<div>${city}, ${zip}</div>
-			`;
-
-			tableBody += `
-				<tr>
-					<th data-label="${headName}" scope="row">${name}</th>
-					<td data-label="${headUser}">${username}</td>
-					<td data-label="${headEmail}">${email}</td>
-					<td data-label="${headAddress}">${address}</td>
-					<td data-label="${headPhone}">${phone}</td>
-				</tr>
-			`;
-		}
-
 		const tbody = document.getElementById("tbody");
 		if (table) {
-			tbody.innerHTML = tableBody;
+			tr.innerHTML = headItems.join(" ");
+			tbody.innerHTML = bodyItems.join(" ");
 		}
 	};
 	request.send();
